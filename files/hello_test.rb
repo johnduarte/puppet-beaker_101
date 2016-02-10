399d26631b16d608a3fae0bf8c546005e0f827af
John Duarte <john.duarte@puppetlabs.com>
2016-02-09 16:22:51 -0800

    Fix hello test to be puppet 4 compatible
    Prior to this commit, the notify resource declared in the
    `hello_test.rb` test was not quoted. This resulted in a type error
    when executed with puppet 4.

--- files/hello_test.rb
+++ files/hello_test.rb
@@ -8,7 +8,7 @@ test_name 'Beaker and puppet hello world test' do
 
   step 'Test Beaker can use puppet on hosts' do
     hosts.each do |host|
-      on(host, puppet('apply', '-e', '"notify { HelloWorld: }"'))
+      on(host, puppet('apply', '-e', '"notify { \'HelloWorld\': }"'))
     end
   end
 
